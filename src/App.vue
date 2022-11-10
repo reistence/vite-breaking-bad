244994ffa316f3a2df523735c1c926742c1d6eef
reistence <gvlamberti@gmail.com>
2022-11-10 14:41:06 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script>
+import axios from "axios";
+import { store } from "./store";
+
+export default {
+  components: {},
+};
+</script>
+
+<template>
+  <h1>yo</h1>
+</template>
+
+<style lang="scss">
+@use "./style/general.scss" as *;
+</style>
