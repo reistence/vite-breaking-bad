cdfc2028f13c48f3169dc54a073b3cd764413766
reistence <gvlamberti@gmail.com>
2022-11-11 17:25:00 +0100

    searchinput btn style on hover

--- src/components/SearchInput.vue
+++ src/components/SearchInput.vue
@@ -15,7 +15,9 @@ export default {
 
 <template>
   <div class="container my-3">
-    <div class="input d-flex justify-content-flex-start">
+    <div
+      class="input d-sm-flex-colum d-md-flex d-lg-flexjustify-content-flex-start"
+    >
       <input
         type="text"
         id="searchKey"
@@ -51,18 +53,23 @@ export default {
 @use "../style/partials/variables" as *;
 
 .input {
-  gap: 2em;
+  gap: 1em;
   align-items: center;
+  flex-wrap: wrap;
   input {
     padding: 0.5em;
     border: none;
     border-radius: 5px;
     width: 20em;
+    margin-bottom: 0.5em;
+    margin-right: 1em;
   }
   select {
     border: none;
     padding: 0.5em;
     border-radius: 5px;
+    margin-bottom: 0.5em;
+    margin-right: 1em;
   }
   button {
     padding: 0.7em 2em;
@@ -71,6 +78,12 @@ export default {
     color: white;
     background-image: url("../assets/img/76a6c6515947b290b00bc2efd3f00833.png");
     background-size: cover;
+    margin-bottom: 0.5em;
+    transition: 300ms;
+    &:hover {
+      border: 1px solid $card-txt;
+      transform: scale(1.05);
+    }
   }
 }
 </style>
